4676220b00de562c23553644fee4e7aa5f57e138
RichardGarita <richard.garita@ucr.ac.cr>
2023-09-30 13:07:30 -0600

    Se adapta el modelo para incluir validaciones y se agrega crearPais al controlador #Laboratorio#6

--- Laboratorio5/Laboratorio5/Models/PaisModel.cs
+++ Laboratorio5/Laboratorio5/Models/PaisModel.cs
@@ -1,10 +1,23 @@
-﻿namespace Laboratorio5.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Laboratorio5.Models
 {
     public class PaisModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Debe proporcionar un nombre")]
+        [DisplayName("Nombre del país: ")]
         public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Debe seleccionar un continente")]
+        [DisplayName("Continente: ")]
         public string Continente { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar el idioma")]
+        [DisplayName("Idioma: ")]
+        [RegularExpression("^[a-zA-Z]+$", ErrorMessage ="No puede ingresar números")]
         public string Idioma { get; set; }
     }
 }
